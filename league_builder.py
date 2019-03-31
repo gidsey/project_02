0c5df95b6356117dc90e1f2705984f03306a293e
chrisguy <chris@chrisguy.co>
2019-03-31 17:50:19 +0100

    Shortened team assignment code

--- league_builder.py
+++ league_builder.py
@@ -25,32 +25,23 @@ if __name__ == "__main__":
 
     def make_teams():
         """Assign players into mixed ability teams."""
-        # Create a separate list of experineced platers and amateur players
 
+        # Create a separate list of experineced platers and amateur players
         for item in players.items():
             player_info = ', '.join([item[0], item[1][0], item[1][1]])
             if (item[1][0]) == 'YES':
                 exp_players.append(player_info)
             else:
                 ama_players.append(player_info)
-            # print("\nexp_players = {}\n".format(exp_players))
-        # print("\nama_players = {}\n".format(ama_players))
 
         # Assign an equal number of exp and amateur plater to ech team
         for player in exp_players:
             sharks.append(exp_players.pop())
-            raptors.append(exp_players.pop())
-            dragons.append(exp_players.pop())
-
-        for player in ama_players:
             sharks.append(ama_players.pop())
+            raptors.append(exp_players.pop())
             raptors.append(ama_players.pop())
+            dragons.append(exp_players.pop())
             dragons.append(ama_players.pop())
-
-
-        # print("\nDragons = {}\n".format(dragons))
-        # print("\nSharks = {}\n".format(sharks))
-        # print("\nRaptors = {}\n".format(raptors))
 
 
     def write_List():
